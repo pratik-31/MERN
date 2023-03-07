e56d222e8c03bed7ab53d7e501e70850354a8e88
Nitu Kumari <78076309+U220053@users.noreply.github.com>
2023-03-08 00:19:19 +0530

    till 3:30

--- backend/models/productModels.js
+++ backend/models/productModels.js
@@ -59,6 +59,12 @@ const productSchema = new mongoose.Schema({
       },
     },
   ],
+
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,13 +3,25 @@ const {
   registerUser,
   loginUser,
   logout,
+  forgotPassword,
+  resetPassword,
+  getUserDetails,
 } = require("../controllers/userController");
+
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 
 router.route("/login").post(loginUser);
 
+router.route("/password/forgot").post(forgotPassword);
+
+router.route("/password/reset/:token").put(resetPassword);
+
 router.route("/logout").get(logout);
+
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
 
 module.exports = router;
